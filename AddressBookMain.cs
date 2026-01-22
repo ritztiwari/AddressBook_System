38931ad2dd4f4dfc6014a46d2730999270e9ed2e
Rishabh Tiwari <mrishabh532@gmail.com>
2026-01-23 01:28:58 +0530

    [Rishabh]. add implementation to create a Contact in Address Book

--- AddressBookMain.cs
+++ AddressBookMain.cs
@@ -7,6 +7,12 @@ namespace AddressBook.AddressBook
         internal static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Address Book");
+
+            //taking the input from parameterised constructor.
+            Contact contact = new Contact("rishabh","tiwari","Block B","ghaziabad","uttar pradesh",201009,"4587695358","abc@gmail.com");
+
+            // displaying the contact details
+            contact.Display();
         }
     }
 }
