4b86f3ec9d78e0ca7bd6ac86652416536bb9fbd7
Rishabh Tiwari <mrishabh532@gmail.com>
2026-01-22 16:55:23 +0530

    [Rishabh]. Initial Commit

---
+++ AddressBookMain.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AddressBook.AddressBook
+{
+    internal class AddressBookMain
+    {
+        internal static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Address Book");
+        }
+    }
+}
